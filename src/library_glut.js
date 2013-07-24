82f1bad728a3f0e7fdf2a6d1f9545ab801062558
Vladimir Vukicevic <vladimir@pobox.com>
2013-07-24 14:59:09 -0400

    Relocate GL procs so that they can be implemented in C but the library version still be accessed through a function pointer, and always expose symbols through egl/glGetProcAddress even if GL emulation is disabled.

--- src/library_glut.js
+++ src/library_glut.js
@@ -275,7 +275,7 @@ var LibraryGLUT = {
     
     Browser.resizeListeners.push(function(width, height) {
       if (GLUT.reshapeFunc) {
-      	Runtime.dynCall('vii', GLUT.reshapeFunc, [width, height]);
+        Runtime.dynCall('vii', GLUT.reshapeFunc, [width, height]);
       }
     });
 
@@ -424,6 +424,7 @@ var LibraryGLUT = {
   },
 
   glutInitDisplayMode: function(mode) {},
+  glutInitDisplayString: function(modestr) {},
   glutSwapBuffers: function() {},
 
   glutPostRedisplay: function() {
